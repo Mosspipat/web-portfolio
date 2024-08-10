1259397dfc3f8d437828c1c878d6797cd749522e
mossPipat <pt_moss17782@hotmail.com>
2024-08-10 16:38:55 +0700

    feat(code):refactor code

--- src/components/SkillItem/SkillItem.tsx
+++ src/components/SkillItem/SkillItem.tsx
@@ -8,7 +8,6 @@ export const SkillItem = ({
   name,
   level,
   iconElement,
-  className,
 }: Readonly<SkillItemProps>) => {
   useEffect(() => {
     gsap.fromTo(
@@ -28,7 +27,7 @@ export const SkillItem = ({
 
   return (
     <div
-      className={`skill-item flex flex-col gap-1 items-center py-3  rounded-lg ease-in-out duration-300 hover:scale-110 hover:shadow-2xl	${className}`}
+      className={`skill-item flex flex-col gap-1 items-center py-3 px-4  rounded-lg ease-in-out duration-300 hover:scale-110 hover:shadow-2xl shadow-md`}
     >
       {iconElement || null}
       <div className="skill-info text-black flex flex-col items-center gap-1 text-center">
--- src/constants/skillSection.tsx
+++ src/constants/skillSection.tsx
@@ -15,37 +15,30 @@ import {
 } from "react-icons/si";
 import { TbBrandReactNative } from "react-icons/tb";
 
-const classNameLanguageSkill = `border-2 border-sky-300 w-[160px]`;
-
 export const languageProgrammingSkill: SkillItemProps[] = [
   {
     name: "JavaScript",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <IoLogoJavascript size={100} color="#f0dc4e" />,
   },
   {
     name: "HTML",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <FaHtml5 size={100} color="#e44c27" />,
   },
   {
     name: "CSS",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <IoLogoCss3 size={100} color="#1775bc" />,
   },
   {
     name: "TypeScript",
     level: "Intermediate",
-    className: classNameLanguageSkill,
     iconElement: <SiTypescript size={100} color="#007bcd" />,
   },
   {
     name: "C#",
     level: "Intermediate",
-    className: classNameLanguageSkill,
     iconElement: <CSharpIcon />,
   },
 ];
@@ -54,43 +47,36 @@ export const toolsSkill: ToolItemProps[] = [
   {
     name: "React JS",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <TbBrandReactNative size={100} color="#60dafb" />,
   },
   {
     name: "Vue.js",
     level: "Beginner",
-    className: classNameLanguageSkill,
     iconElement: <FaVuejs size={100} color="#40b882" />,
   },
   {
     name: "Next.js",
     level: "Intermediate",
-    className: classNameLanguageSkill,
     iconElement: <NextJSIcon />,
   },
   {
     name: "git",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <FaGitSquare size={100} color="#f05030" />,
   },
   {
     name: "nodeJS",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <FaNode size={100} color="#89bc5c" />,
   },
   {
     name: "wordPress",
     level: "Beginner",
-    className: classNameLanguageSkill,
     iconElement: <FaWordpress size={100} color="#00769d" />,
   },
   {
     name: "commandLine",
     level: "Beginner",
-    className: classNameLanguageSkill,
     iconElement: <HiCommandLine size={100} color="#202020" />,
   },
 ];
@@ -99,19 +85,16 @@ export const gameEngineToolsSkill: ToolItemProps[] = [
   {
     name: "UnityEngine",
     level: "Advanced",
-    className: classNameLanguageSkill,
     iconElement: <FaUnity size={100} color="#202020" />,
   },
   {
     name: "UnrealEngine",
     level: "Intermediate",
-    className: classNameLanguageSkill,
     iconElement: <SiUnrealengine size={100} color="#2e2e2e" />,
   },
   {
     name: "Construct 3",
     level: "Intermediate",
-    className: classNameLanguageSkill,
     iconElement: <SiConstruct3 size={100} color="#d2e2f0" />,
   },
 ];
@@ -119,7 +102,6 @@ export const otherToolsSkill: ToolItemProps[] = [
   {
     name: "Sparkar",
     level: "Intermediate",
-    className: classNameLanguageSkill,
     iconElement: <SiSparkar size={100} color="#202020" />,
   },
 ];
