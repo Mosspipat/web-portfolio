95ee9d5c57159987461de667168d2b31a6c87c95
mossPipat <pt_moss17782@hotmail.com>
2024-08-13 18:56:16 +0700

    style(section): style introduct component

---
+++ src/data/Sections/index.ts
@@ -0,0 +1,1 @@
+export { dataProfile } from "./IntroSection";
--- src/sections/IntroduceSection.tsx
+++ src/sections/IntroduceSection.tsx
@@ -1,18 +1,8 @@
+import { Divider } from "@/components";
+import { dataProfile } from "@/data";
 import React from "react";
 
 export const IntroduceSection = () => {
-  type DataProfile = {
-    key: string;
-    value: string;
-  };
-
-  const dataProfile: DataProfile[] = [
-    { key: "Name:", value: "Pipat Horakungthong" },
-    { key: "Email:", value: "pt_moss17782@hotmail.com" },
-    { key: "Age:", value: "28" },
-    { key: "From:", value: "Bangkok, Thailand" },
-  ];
-
   const ItemProfileDetail = ({
     title,
     des,
@@ -21,8 +11,8 @@ export const IntroduceSection = () => {
     des: string;
   }) => {
     return (
-      <div className="">
-        <div className="text-base font-semibold">{title}</div>
+      <div className="flex flex-col  pt-2">
+        <div className="text-base font-semibold text-blue-600">{title}</div>
         <div>{des}</div>
       </div>
     );
@@ -33,11 +23,11 @@ export const IntroduceSection = () => {
       <p className="text-center text-6xl font-bold">To get to know me better</p>
       <p>
         I'm
-        <span className="text-blue-400 ml-1 text-4xl font-semibold tracking-wide">
+        <span className="text-blue-600 ml-1 text-4xl font-semibold tracking-wide">
           Web Developer{" "}
         </span>
         and
-        <span className="text-blue-400 ml-1 text-4xl font-semibold tracking-wide">
+        <span className="text-blue-600 ml-1 text-4xl font-semibold tracking-wide">
           Game Developer
         </span>
       </p>
@@ -55,20 +45,20 @@ export const IntroduceSection = () => {
           </div>
           <div className="flex justify-center gap-10 ">
             <div>
-              <div className="text-center text-blue-400 text-4xl font-bold">
+              <div className="text-center text-blue-600 text-4xl font-bold">
                 5
               </div>
               <div>Years experience</div>
             </div>
             <div>
-              <div className="text-center text-blue-400 text-4xl font-bold">
+              <div className="text-center text-blue-600 text-4xl font-bold">
                 4
               </div>
               <div>Programmer language Skill</div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-2 ">
+        <div className="flex flex-col gap-3 divide-y divide-dashed divide-blue-100">
           {dataProfile.map((item, index) => {
             return (
               <ItemProfileDetail
