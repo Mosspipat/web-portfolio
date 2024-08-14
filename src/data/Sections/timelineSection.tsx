053413c8ca0c4e37448b776f89c94f26f8a96475
mossPipat <pt_moss17782@hotmail.com>
2024-08-14 22:09:35 +0700

    feat(mockData): moack data for timeline section

--- src/data/Sections/timelineSection.tsx
+++ src/data/Sections/timelineSection.tsx
@@ -1,40 +1,54 @@
+import React from "react";
+import { MdSchool, MdWork } from "react-icons/md";
+
 export const timelineData: renderLIProps[] = [
   {
     year: "2002-2014",
-    title:
-      "High School Certificate equivalent from Assumption College Thonburi (ACT)",
+    schoolOrCompany: "Assumption College Thonburi (ACT)",
+    JobOrDepartment: "High School Certificate equivalent ",
     description:
       "High School Certificate equivalent at Assumption College Thonburi (ACT)",
+    iconItem: <MdSchool />,
   },
   {
     year: "2014-2018",
-    title: "Bachelor's degree from University of Technology Thonburi (KMUTT)",
+    schoolOrCompany: "University of Technology Thonburi (KMUTT)",
+    JobOrDepartment: "Bachelor's degree",
     description:
       "Bachelor's degree at King Mongkut's University of Technology Thonburi (KMUTT),Bangkok Media digital technology (MDT)",
+    iconItem: <MdSchool />,
   },
   {
     year: "2018-2020",
-    title: "Anyi media Interactive media and Game developer ",
+    schoolOrCompany: "Anyi media",
+    JobOrDepartment: " Interactive media and Game developer ",
     description:
       "developer 3D interactive with game engine.Experience with the most exclusive between interactive and online events.",
+    iconItem: <MdWork />,
   },
   {
     year: "2020-2022",
-    title: "VIZ Studio Web developer",
+    schoolOrCompany: "VIZ Studio",
+    JobOrDepartment: "Web developer",
     description:
       "develop website with AR experience and filter effect to share in social platform.create game content with game engine.",
+    iconItem: <MdWork />,
   },
   {
     year: "2023-Present",
-    title: "MagicBox solution Frontend developer",
+    schoolOrCompany: "MagicBox solution",
+    JobOrDepartment: "Frontend developer",
     description:
       "Develop and maintain both website and mobile applications using a frontend framework, ensuring seamless integration between the back office and the main application with the frontend.",
+    iconItem: <MdWork />,
   },
 ];
 
 export type renderLIProps = {
   year: string;
-  title: string;
+  JobOrDepartment: string;
+  schoolOrCompany?: string;
   description: string;
   link?: string;
+  iconItem?: React.ReactElement;
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,13 +12,39 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
-  plugins: [addVariablesForColors],
+  plugins: [
+    addVariablesForColors,
+    function ({ addUtilities }: { addUtilities: any }) {
+      addUtilities(
+        {
+          ".text-shadow-sm": {
+            textShadow: "1px 1px 1px rgba(0, 0, 0, 0.3)",
+          },
+          ".text-shadow": {
+            textShadow: "2px 2px 2px rgba(0, 0, 0, 0.3)",
+          },
+          ".text-shadow-md": {
+            textShadow: "4px 4px 4px rgba(0, 0, 0, 0.3)",
+          },
+          ".text-shadow-lg": {
+            textShadow: "6px 6px 6px rgba(0, 0, 0, 0.3)",
+          },
+        },
+        ["responsive", "hover"]
+      );
+    },
+  ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        text: "0 0 1px rgba(0, 0, 0, 0.1), 0 0 1px rgba(0, 0, 0, 0.1)",
+        "text-lg": "0 0 2px rgba(0, 0, 0, 0.15), 0 0 2px rgba(0, 0, 0, 0.15)",
+        "text-xl": "0 0 4px rgba(0, 0, 0, 0.2), 0 0 4px rgba(0, 0, 0, 0.2)",
       },
     },
   },
