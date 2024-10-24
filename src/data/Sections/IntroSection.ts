3e21cf544eb142fc5d6cad9cb224eefcbea374ec
mossPipat <pt_moss17782@hotmail.com>
2024-10-24 14:14:11 +0700

    chore(content): change to age 28 to 29

--- src/data/Sections/IntroSection.ts
+++ src/data/Sections/IntroSection.ts
@@ -6,6 +6,6 @@ type DataProfile = {
 export const dataProfile: DataProfile[] = [
   { key: "Name:", value: "Pipat Horakungthong" },
   { key: "Email:", value: "pt_moss17782@hotmail.com" },
-  { key: "Age:", value: "28" },
+  { key: "Age:", value: "29" },
   { key: "From:", value: "Bangkok, Thailand" },
 ];
