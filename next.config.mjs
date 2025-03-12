ea0658bb04c5c499f3b13e3499a0af5c8c056b2a
mossPipat <pt_moss17782@hotmail.com>
2025-03-12 14:33:39 +0700

    fix(deploy): redirect main route is to/portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/dashboard',
+        source: '/',
         destination: '/portfolio',
         permanent: true,
       },
